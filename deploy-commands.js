9670ea87d471e2d7c4523c117d8d9bbf9548ae2e
yiota <killourdreams06@gmail.com>
2024-05-21 16:31:26 +0300

    added bc it was wrong before

---
+++ deploy-commands.js
@@ -0,0 +1,36 @@
+const { REST, Routes } = require('discord.js');
+const secret = require("./secret.json")
+
+const filePath = ['./kill.js', './schedule.js'];
+
+const commands = []
+
+for (const path of filePath){
+    const command = require(path);
+    commands.push(command.data.toJSON());
+}
+
+const clientID = "1228731205382574131";
+const guildID = "864966978975301653";
+
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(secret);
+
+// and deploy your commands!
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationCommands(clientID),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+})();
